b95bd6070cf73b15057dc152b0701106646ffa17
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-06 18:05:11 -0600

    Adds print_message method

---
+++ lib/night_writer.rb
@@ -0,0 +1,7 @@
+class NightWriter
+
+  def print_message
+    "Created braille.txt containing 256 characters"
+  end
+  
+end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -4,6 +4,14 @@ require "./lib/night_writer"
 
 class NightWriterTest < Minitest::Test
 
+  def test_it_exists
+    nw = NightWriter.new
+    assert_instance_of NightWriter, nw
+  end
 
+  def test_message_prints_to_terminal
+    nw = NightWriter.new
+    assert_equal "Created braille.txt containing 256 characters", nw.print_message
+  end
 
 end
